2385099a069a473ee9f217ea2c46746d34e95d63
Thomas Kellough <thomas.r.kellough@gmail.com>
2019-11-25 15:37:36 -0600

    created popup for holiday party event

---
+++ marigoldstudio/static/js/popupscripts.js
@@ -0,0 +1,31 @@
+function snowfall(){
+    var total = 500 + Math.random()*100;
+    for (i=0; i<total; i++){
+        $('body').append("<div class=snowflakes id="+i+"> </div>");
+
+        $('#'+i).css({
+        animationDelay:Math.random()*20+'s',
+        left:Math.random()*100+'%'
+        });
+    }
+}
+
+function clearSnow() {
+    $('.snowflakes').hide();
+}
+
+function openPopup() {
+    $('.popup').show();
+    $('body > *').wrap('<div class="blur-all">');
+    $('.popup').unwrap();
+    snowfall();
+}
+
+function closePopup() {
+
+    $('.popup').hide();
+    $('.blur-all').children().unwrap();
+    clearSnow();
+}
+setTimeout(openPopup, 3000);
+$('#closepopup').click(closePopup);
